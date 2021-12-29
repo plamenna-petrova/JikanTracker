1068be1913b7bdc3d258e66c70edcdcd11d232c9
plamenna.petrova <plamennavp@abv.bg>
2021-12-29 09:58:57 -0800

    Added basic Firebase Authentication and implemented register, login and logout functionality with view navigation

--- JikanTracker/ContentView.swift
+++ JikanTracker/ContentView.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import FirebaseAuth
 import CoreData
 import UIKit
 
@@ -43,7 +44,7 @@ struct CustomGroup:View {
     }
 }
 
-struct ContentView: View {
+struct JikanTrackerView: View {
     @Environment(\.managedObjectContext) var moc
     @FetchRequest(entity: Manga.entity(), sortDescriptors: [
         NSSortDescriptor(keyPath: \Manga.name, ascending: true),
@@ -61,9 +62,10 @@ struct ContentView: View {
         NSSortDescriptor(keyPath: \Movie.name, ascending: true),
         NSSortDescriptor(keyPath: \Movie.rating, ascending: true)
     ]) var movies : FetchedResults<Movie>
-
+    
+    @EnvironmentObject var viewModel: AppViewModel
+    
     var body: some View {
-        NavigationView {
             ScrollView{
                 VStack{
                     LazyVGrid(columns: [.init(), .init()]){
@@ -111,10 +113,183 @@ struct ContentView: View {
                             }
                         }.padding()
                     }
-                }
+                    Button(action: {
+                        viewModel.signOut()
+                    }, label: {
+                        Text("Sign Out")
+                            .frame(width: 200, height: 50)
+                            .background(Color.blue)
+                            .foregroundColor(Color.white)
+                            .padding()
+                    })                }
                 .navigationTitle("Jikan Tracker Lists")
             }
         }
+}
+
+class AppViewModel : ObservableObject {
+    
+    let auth = Auth.auth()
+    
+    @Published var signedIn = false
+    
+    var isSignedIn: Bool {
+        return auth.currentUser != nil
+    }
+    
+    func signIn(email: String, password: String) {
+        auth.signIn(withEmail: email, password: password) { [weak self] result, error in
+            guard result != nil, error == nil else {
+                return
+            }
+           
+            DispatchQueue.main.async {
+                // Success
+                self?.signedIn = true
+            }
+        }
+    }
+    
+    func signUp(email: String, password: String) {
+        auth.createUser(withEmail: email, password: password) { [weak self] result, error in
+            guard result != nil, error == nil else {
+                return
+            }
+            
+            DispatchQueue.main.async {
+                // Success
+                self?.signedIn = true
+            }
+        }
+    }
+    
+    func signOut() {
+        try? auth.signOut()
+        
+        self.signedIn = false
+    }
+    
+}
+
+struct ContentView: View {
+    @EnvironmentObject var viewModel: AppViewModel
+    
+    var body: some View {
+        NavigationView {
+            if viewModel.signedIn {
+                JikanTrackerView()
+            } else {
+                SignInView()
+            }
+        }
+        .onAppear{
+            viewModel.signedIn = viewModel.isSignedIn
+        }
+    }
+}
+
+struct SignInView: View {
+    @State var email = ""
+    @State var password = ""
+    
+    @EnvironmentObject var viewModel: AppViewModel
+    
+    var body: some View {
+        VStack {
+                Image("logo")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 150, height: 150)
+                
+                VStack {
+                    TextField("Email Address", text: $email)
+                        .disableAutocorrection(true)
+                        .autocapitalization(.none)
+                        .padding()
+                        .background(Color(.secondarySystemBackground))
+                    
+                    SecureField("Password", text: $password)
+                        .disableAutocorrection(true)
+                        .autocapitalization(.none)
+                        .padding()
+                        .background(Color(.secondarySystemBackground))
+                    
+                    Button(action: {
+                        
+                        guard !email.isEmpty, !password.isEmpty else {
+                            return
+                        }
+                        
+                        viewModel.signIn(email: email, password: password)
+                        
+                    }, label: {
+                        Text("Sign In")
+                            .foregroundColor(Color.white)
+                            .frame(width: 200, height: 50)
+                            .cornerRadius(8)
+                            .background(Color.blue)
+                    })
+                    .padding()
+                    
+                    NavigationLink("Create Account", destination: SignUpView())
+                        .padding()
+                }
+                .padding()
+                
+                Spacer()
+            }
+            .navigationTitle("Sign In")
+    }
+}
+
+struct SignUpView: View {
+    @State var email = ""
+    @State var password = ""
+    
+    @EnvironmentObject var viewModel: AppViewModel
+    
+    var body: some View {
+        VStack {
+                Image("logo")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 150, height: 150)
+                
+                VStack {
+                    TextField("Email Address", text: $email)
+                        .disableAutocorrection(true)
+                        .autocapitalization(.none)
+                        .padding()
+                        .background(Color(.secondarySystemBackground))
+                    
+                    SecureField("Password", text: $password)
+                        .disableAutocorrection(true)
+                        .autocapitalization(.none)
+                        .padding()
+                        .background(Color(.secondarySystemBackground))
+                    
+                    Button(action: {
+                        
+                        guard !email.isEmpty, !password.isEmpty else {
+                            return
+                        }
+                        
+                        viewModel.signUp(email: email, password: password)
+                        
+                    }, label: {
+                        Text("Create Account")
+                            .foregroundColor(Color.white)
+                            .frame(width: 200, height: 50)
+                            .cornerRadius(8)
+                            .background(Color.blue)
+                    })
+                    
+                }
+                .padding()
+                
+                Spacer()
+            }
+            .navigationTitle("Create Account")
     }
 }
 
--- JikanTracker/JikanTrackerApp.swift
+++ JikanTracker/JikanTrackerApp.swift
@@ -6,15 +6,30 @@
 //
 
 import SwiftUI
+import Firebase
 
 @main
 struct JikanTrackerApp: App {
+    
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    
     let persistenceController = PersistenceController.shared
 
     var body: some Scene {
         WindowGroup {
+            let viewModel = AppViewModel()
             ContentView()
                 .environment(\.managedObjectContext, persistenceController.container.viewContext)
+                .environmentObject(viewModel)
         }
+    }
+}
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        
+        FirebaseApp.configure()
+        
+        return true
     }
 }
